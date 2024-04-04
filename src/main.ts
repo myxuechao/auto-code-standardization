c956f467cf01b92add43a9049a296b9c36da1514
LiuXueChao <482113948@qq.com>
2024-04-05 02:28:25 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: LiuXueChao
+ * @Date: 2024-04-03 13:28:45
+ * @LastEditors: your name
+ * @LastEditTime: 2024-04-05 02:10:15
+ * @Description: file contclg
+ */
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+createApp(App).mount("#app");
+console.log("测试使用czg");
