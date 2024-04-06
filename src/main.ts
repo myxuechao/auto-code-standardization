0c90a6e62eee3e1a614a474893ba329adc7118f2
LiuXueChao <482113948@qq.com>
2024-04-06 22:21:17 +0800

    fix: deletelog

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@
  * @Author: LiuXueChao
  * @Date: 2024-04-03 13:28:45
  * @LastEditors: your name
- * @LastEditTime: 2024-04-05 02:10:15
+ * @LastEditTime: 2024-04-06 22:21:02
  * @Description: file contclg
  */
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 createApp(App).mount("#app");
-console.log("测试使用czg");
